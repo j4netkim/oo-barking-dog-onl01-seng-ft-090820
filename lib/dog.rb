82cca72dc16612ae00e82882d3816096533e98ac
J K <kj688895@gmail.com>
2020-09-20 21:12:08 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,14 @@
 class Dog
 
-  def .name=(dogs_name)
+  def name=(dogs_name)
     doggies_name = dogs_name
   end
 
-  def .name
+  def name
     doggies_name
   end
 
   def bark
     puts "woof"
   end
-end 
+end
